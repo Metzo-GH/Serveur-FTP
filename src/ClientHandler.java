a194f05481ee90cc05be45606745c051aa691fc5
Metzo-GH <mouhamedhino@gmail.com>
2024-01-11 21:43:29 +0100

    final

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,79 +1,71 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientHandler implements Runnable {
-    private Socket clientSocket;
-    private BufferedReader in;
-    private PrintWriter out;
+    private Socket socket;
 
     public ClientHandler(Socket socket) {
-        this.clientSocket = socket;
+        this.socket = socket;
     }
 
     @Override
     public void run() {
         try {
-            // Flux de lecture et écriture pour communiquer avec le client
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+            Scanner scanner = new Scanner(in);
 
-            // Envoyer le message "220 Service ready" une fois la connexion établie
-            out.println("220 Service ready");
+            out.write("220 Service ready".getBytes());
 
-            // Demander le nom d'utilisateur
-            out.println("331 User name ok, need password");
-            String login = in.readLine().trim();
-            System.out.println(login);
+             String login = scanner.nextLine();
+            out.write("331 User name ok, need password".getBytes());
+            out.write(login.getBytes());
 
-            // Demander le mot de passe
-            out.println("331 Enter password");
-            String password = in.readLine().trim();
-            System.out.println(password);
+            out.write("331 Enter password".getBytes());
+            String password = scanner.nextLine();
+            out.write(password.getBytes());
 
-            // Vérifier les informations d'identification
             if (isValidUser(login, password)) {
-                out.println("230 User logged in");
+                out.write("230 User logged in".getBytes());
             } else {
-                out.println("530 Login incorrect");
+                out.write("530 Login incorrect".getBytes());
+                scanner.close();
                 return;
             }
 
-            // Boucle de gestion des commandes FTP
+
             String clientCommand;
             do {
-                out.println("220 Entrez une commande (quit pour se déconnecter): ");
-                clientCommand = in.readLine().trim();
+                out.write("220 Entrez une commande (quit pour se déconnecter): ".getBytes());
+                clientCommand = scanner.nextLine();
 
-                // Implémenter la logique pour les commandes FTP
                 switch (clientCommand.toLowerCase()) {
                     case "quit":
                         System.out.println("221 Déconnexion.");
                         break;
                     case "get":
-                        // Ajoutez ici la logique pour la commande 'get'
-                        // out.println("200 Commande GET traitée avec succès");
+
                         break;
                     case "dir":
-                        // Ajoutez ici la logique pour la commande 'dir'
-                        // out.println("200 Commande DIR traitée avec succès");
+
                         break;
                     case "cd":
-                        // Ajoutez ici la logique pour la commande 'cd'
-                        // out.println("200 Commande CD traitée avec succès");
+
                         break;
                     default:
-                        out.println("500 Commande non reconnue.");
+                        out.write("500 Commande non reconnue.".getBytes());
                 }
 
             } while (!clientCommand.equalsIgnoreCase("quit"));
 
-            // Fermer les flux et la connexion
+
             in.close();
             out.close();
-            clientSocket.close();
+            socket.close();
+            scanner.close();
 
         } catch (IOException e) {
             System.err.println("Erreur : " + e);
@@ -82,7 +74,6 @@ public class ClientHandler implements Runnable {
     }
 
     private boolean isValidUser(String login, String password) {
-        // Vérifier les informations d'identification ici (à remplacer par une logique réelle)
         return login.equals("USER metzo") && password.equals("PASS a");
     }
 }
