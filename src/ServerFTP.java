ec69af22ec1b559985efeb12c5f653b966412f18
Metzo-GH <145110616+Metzo-GH@users.noreply.github.com>
2024-01-18 11:17:35 +0100

    get implementation

--- src/ServerFTP.java
+++ src/ServerFTP.java
@@ -59,7 +59,8 @@ public class ServerFTP {
                             closeCon(scanner, in, out, socket,serverSocket);
                             break;
                         case "get":
-                        
+                            String fileName = scanner.nextLine();
+                            getFile(fileName, out);
                             break;
                         case "dir":
                 
@@ -68,7 +69,7 @@ public class ServerFTP {
 
                             break;
                         default:
-                            out.write("500 Commande non reconnue.\r\n".getBytes());
+                            out.write("500 Commande non reconnue\r\n".getBytes());
                     }
                 
                 } while (!clientCommand.equalsIgnoreCase("quit"));
@@ -92,5 +93,42 @@ public class ServerFTP {
         out.close();
         serverSocket.close();
         socket.close();
+    }
+
+
+    private static void getFile(String fileName, OutputStream out) {
+        try {
+            File file = new File("storage/"+fileName);
+
+            if (!file.exists()) {
+                out.write("550 File not found!".getBytes());
+            } else {
+                out.write("200 PORT command successful".getBytes());
+                out.write(( "150 data connection for "+file.getName() ).getBytes());
+                FileInputStream fileInputStream = new FileInputStream(file);
+            
+                byte[] buffer = new byte[1024];
+                int byteRead;
+
+                try {
+                    while ((byteRead = fileInputStream.read(buffer, 0, 1024)) != -1) {
+                        out.write(buffer, 0, byteRead);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error : " + e);
+                    e.printStackTrace();
+                }
+                
+                fileInputStream.close();
+                out.write( ("Completed file transmission of "+file.getName()+"\r\n").getBytes() );
+                out.write( ("226 Transfer complete\r\n").getBytes() );
+            }
+            
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Error : " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
